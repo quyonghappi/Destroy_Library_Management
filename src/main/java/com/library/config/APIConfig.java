5352be054b75289f505cb447dc22cb222e312e70
quyonghappi <maianhku12112005@gmail.com>
2024-10-09 17:30:10 +0700

    add api configuration

---
+++ src/main/java/com/library/config/APIConfig.java
@@ -0,0 +1,11 @@
+package com.library.config;
+
+public class APIConfig {
+    //create ur own api key
+    public static final String GOOGLE_BOOKS_API_KEY = "AIzaSyABRuJ-IH-HwQYh9AFoTqRN0h6LD2TqLJc";
+
+    ///database connection details
+    public static final String URL = "jdbc:mysql://localhost:3306/library_management";
+    public static final String USER = "use_yours";
+    public static final String PASSWORD = "use_yours";
+}
