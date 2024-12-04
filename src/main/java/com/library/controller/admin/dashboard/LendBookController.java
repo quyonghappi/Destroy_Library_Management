220a9a003b37fce14736fc62cd3c8ed1b39ef808
ntlinhhhhhh <160174186+ntlinhhhhhh@users.noreply.github.com>
2024-12-04 17:27:29 +0700

    update lend button

--- src/main/java/com/library/controller/admin/dashboard/LendBookController.java
+++ src/main/java/com/library/controller/admin/dashboard/LendBookController.java
@@ -1,15 +1,20 @@
 package com.library.controller.admin.dashboard;
 
 import com.library.dao.BorrowingRecordDao;
+import com.library.dao.DocumentDao;
 import com.library.dao.UserDao;
 import com.library.models.BorrowingRecord;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+
+import static com.library.controller.start.LoadView.showAlert;
 
 public class LendBookController {
 
@@ -31,15 +36,55 @@ public class LendBookController {
     @FXML
     private void addLentBook(ActionEvent event) {
         String username=usernameField.getText();
-        String[] isbn=bookDetailsField.getText().split(",");
-        for (int i=0;i<isbn.length;i++) {
-            try {
-            String userId = String.valueOf(UserDao.findUserByName(username).getUserId());
-            LocalDateTime borrowDate=LocalDateTime.now();
-            borrowingRecordDao.add(new BorrowingRecord(userId,isbn[i].trim(),borrowDate, "borrowed"));
-            } catch(Exception e) {
-                e.printStackTrace();
+        String bookId = bookDetailsField.getText();
+
+        if (username.isEmpty() || bookId.isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, "Validation Error", "Please fill in all fields.");
+            return;
+        }
+
+
+
+
+
+
+        try {
+
+            if (UserDao.findUserByName(username) == null) {
+                showAlert(Alert.AlertType.ERROR, "Validation Error", "User not found: " + username);
+                return;
             }
+
+            if (DocumentDao.findByIsbn(bookId) == null) {
+                showAlert(Alert.AlertType.ERROR, "Validation Error", "Book not found: " + bookId);
+                return;
+            }
+
+            Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+            confirmationAlert.setTitle("Confirm Lent Book");
+            confirmationAlert.setHeaderText("Confirm Book Details");
+            confirmationAlert.setContentText("Are you sure you want to lent the book: \n\n" +
+                    "User Name: " + username + "\n" +
+                    "BookDetails: " + bookId);
+            confirmationAlert.showAndWait().ifPresent(response -> {
+                if (response == ButtonType.OK) {
+                    try {
+//
+                        String userId = String.valueOf(UserDao.findUserByName(username).getUserId());
+                        LocalDateTime borrowDate=LocalDateTime.now();
+                        borrowingRecordDao.add(new BorrowingRecord(userId,bookId.trim(),borrowDate, "borrowed"));
+                        showAlert(Alert.AlertType.INFORMATION, "Lent Book", "Lent Book Successfully");
+                        usernameField.clear();
+                        bookDetailsField.clear();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        showAlert(Alert.AlertType.ERROR, "Error", "Failed to add book. Please try again.");
+                    }
+                }
+            });
+
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
